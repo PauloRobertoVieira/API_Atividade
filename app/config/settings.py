9085ee258ba63116d7f27016348e59b454592439
PR_Vieira <paulovieira@discente.ufg.br>
2025-02-15 15:38:12 -0300

    feat(settings) inclusão da api_secret no arquivo .env

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,12 +1,14 @@
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
+
 load_dotenv()
 
 class Settings(BaseSettings):
     openai_api_key: str
+    api_secret_token: str
 
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
 
-settings = Settings()
+settings = Settings() 
--- app/utils/security.py
+++ app/utils/security.py
@@ -1,11 +1,13 @@
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from fastapi import HTTPException, status
+from app.config.settings import settings
 
 security = HTTPBearer()
 
 
+
 def verify_token(credentials: HTTPAuthorizationCredentials):
-    if credentials.credentials != "secret-token":
+    if credentials.credentials != settings.api_secret_token:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Credenciais inválidas",
